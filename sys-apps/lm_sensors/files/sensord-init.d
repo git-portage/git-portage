b88d28b7527f86d0c51d3ef3c9fee95859421f21
phreak <phreak>
2007-05-17 07:31:41 +0000

    Cleaning up FILESDIR, cleaning up some of the older versions. Fixing the bashisms in the init-script (thanks to Mike Arthur <mike at mikearthur.co.uk> in #178781). (Portage version: 2.1.2.7)

--- sys-apps/lm_sensors/files/sensord-init.d
+++ sys-apps/lm_sensors/files/sensord-init.d
@@ -1,7 +1,7 @@
 #!/sbin/runscript
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/sys-apps/lm_sensors/files/Attic/lm_sensors-2.10.1-sensord-init.d,v 1.1 2006/09/29 15:04:55 phreak Exp $
+# $Header: /var/cvsroot/gentoo-x86/sys-apps/lm_sensors/files/Attic/sensord-init.d,v 1.1 2007/05/17 07:31:41 phreak Exp $
 
 CONFIG=/etc/sensors.conf
 
