8e18729efca501535163d6843b7ee5a2c6e8c117
fauli <fauli>
2008-11-28 13:41:51 +0000

    change split dependencies to better fit the needs of Gramps, see bug 248927 by Matteo Modesti <mattemod AT gmail DOT com> (Portage version: 2.1.4.5)

--- app-emacs/graphviz-dot-mode/files/50graphviz-dot-mode-gentoo.el
+++ app-emacs/graphviz-dot-mode/files/50graphviz-dot-mode-gentoo.el
@@ -1,7 +1,4 @@
 
-;;; graphviz-dot-mode site-lisp configuration
+;;; site-lisp configuration for graphviz-dot-mode
 
 (add-to-list 'load-path "@SITELISP@")
-(autoload 'graphviz-dot-mode "graphviz-dot-mode"
-  "Major mode for the dot language." t)
-(add-to-list 'auto-mode-alist '("\\.dot\\'" . graphviz-dot-mode))
