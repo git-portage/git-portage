0fc67abe7d837d002c0e0de061dec6fa60232e0f
maekke <maekke>
2009-02-14 22:04:06 +0000

    add patch to disable automagic dependencies, bug #254109 (Portage version: 2.2_rc23/cvs/Linux i686)  (Signed Manifest commit)

--- app-emacs/wikipedia-mode/files/50wikipedia-mode-gentoo.el
+++ app-emacs/wikipedia-mode/files/50wikipedia-mode-gentoo.el
@@ -6,5 +6,7 @@
 (autoload 'wikipedia-mode "wikipedia-mode"
   "Major mode for editing documents in Wikipedia markup." t)
 
-(add-to-list 'auto-mode-alist '("\\.wiki\\'" . wikipedia-mode))
-(add-to-list 'auto-mode-alist '("/[a-z]+\\.wikipedia\\.org" . wikipedia-mode))
+(add-to-list 'auto-mode-alist
+  '("\\.wiki\\'" . wikipedia-mode))
+(add-to-list 'auto-mode-alist
+  '("en\\.wikipedia\\.org" . wikipedia-mode))
