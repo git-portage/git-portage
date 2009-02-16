acdb025963c93d9ade2e4d8157c1bf9703ba4af5
tove <tove>
2009-02-16 18:21:58 +0000

    Version bump. Cleanup (Portage version: 2.2_rc23/cvs/Linux i686)  (Signed Manifest commit)

--- app-emacs/distel/files/50distel-gentoo.el
+++ app-emacs/distel/files/50distel-gentoo.el
@@ -4,4 +4,3 @@
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'distel-erlang-mode-hook "distel" nil t)
 (add-hook 'erlang-mode-hook 'distel-erlang-mode-hook)
-(setq distel-ebin-directory "/usr/share/distel/ebin")
