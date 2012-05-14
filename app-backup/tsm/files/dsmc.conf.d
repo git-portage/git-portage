023bd2b89e6bff1d9095f14d5f1de22a30f890cb
pacho <pacho>
2012-05-14 16:12:10 +0000

    Version bump (#390581) that also fixes logrotate config file (#375041), thanks a lot to Martin von Gagern that also becomes maintainer with me as proxy.
    (Portage version: 2.1.10.59/cvs/Linux x86_64)

--- app-backup/tsm/files/dsmc.conf.d
+++ app-backup/tsm/files/dsmc.conf.d
@@ -1,6 +1,6 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
+# Copyright 1999-2012 Gentoo Technologies, Inc.
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/app-backup/tsm/files/dsmc.conf.d,v 1.1 2010/01/26 19:40:32 dertobi123 Exp $
+# $Header: /var/cvsroot/gentoo-x86/app-backup/tsm/files/dsmc.conf.d,v 1.2 2012/05/14 16:12:10 pacho Exp $
 
 # Config file for /etc/init.d/tivoli
 
